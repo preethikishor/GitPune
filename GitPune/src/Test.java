b7b2eba6a6b50fe135e481e1fbd4f84139d34f9d
preethikishor <preethidj@gmail.com>
2018-06-06 19:35:54 +0530

    SUB

--- GitPune/src/Test.java
+++ GitPune/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("addition of two numbers A and B is +sum");
+		System.out.println("Subtraction of two numbers A and B is +sub");
 
 	}
 
