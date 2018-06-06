1af4d2c04463418c47ffb158347f3b2ec7bce8c0
preethikishor <preethidj@gmail.com>
2018-06-06 22:11:15 +0530

    added subtraction

--- GitPune/src/Test.java
+++ GitPune/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("addition of two numbers A and B is +sum");
-		System.out.println("Subtraction of two numbers A and B is +sub");
+		System.out.println("Subtraction of two numbers A and B is +subtraction");
 
 	}
 
