efedd1611b34f7128a4f9f3464d159f9af9c801b
preethikishor <preethidj@gmail.com>
2018-06-06 22:27:11 +0530

    sdsd

--- GitPune/src/Test.java
+++ GitPune/src/Test.java
@@ -6,11 +6,11 @@ public class Test {
 
 
 		
-		System.out.println("addition of two numbers A and B is +suma");
+		System.out.println("addition of two numbers A and B is +sumaa");
 		
 
 		
-		System.out.println("Subtraction of two numbers A and B is +suba");
+		System.out.println("Subtraction of two numbers A and B is +subaa");
 
 
 	}
