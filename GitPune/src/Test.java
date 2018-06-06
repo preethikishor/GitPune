d96923bbc156d0e97a95623709fef38b6b7a017f
preethikishor <preethidj@gmail.com>
2018-06-06 22:33:36 +0530

    sdsd

--- GitPune/src/Test.java
+++ GitPune/src/Test.java
@@ -1,19 +1,12 @@
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
-
-
+System.out.println("addition of two numbers A and B is +Addition%%");
 		
-
-		
-	
-		System.out.println("addition of two numbers A and B is +Addition");
-		
-
-		
-		System.out.println("Subtraction of two numbers A and B is +Subtraction");
+System.out.println("Subtraction of two numbers A and B is +Subtraction%%");
 
 
 
