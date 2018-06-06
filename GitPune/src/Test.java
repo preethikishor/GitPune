a3c3adaea3f9661acb47088851ef7afe1ba1c40d
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-06-06 22:27:45 +0530

    Update Test.java

--- GitPune/src/Test.java
+++ GitPune/src/Test.java
@@ -6,11 +6,11 @@ public class Test {
 
 
 		
-		System.out.println("addition of two numbers A and B is +suma");
+		System.out.println("addition of two numbers A and B is +Addition");
 		
 
 		
-		System.out.println("Subtraction of two numbers A and B is +suba");
+		System.out.println("Subtraction of two numbers A and B is +Subtraction");
 
 
 	}
