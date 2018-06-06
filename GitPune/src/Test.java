7cf91b014fbe39950a09d8af8b54ee05d69859c8
preethikishor <preethidj@gmail.com>
2018-06-06 19:26:42 +0530

    dfdf

--- GitPune/src/Test.java
+++ GitPune/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("addition of two numbers");
+		System.out.println("addition of two numbers a and b is ");
 
 	}
 
